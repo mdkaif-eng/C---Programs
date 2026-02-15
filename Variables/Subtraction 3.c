40458be1d91f011dcb83911ea32e68bdd0190e91
Md Kaif <mdkaif.kmclu@gmail.com>
2026-02-15 12:28:36 +0530

    Subtraction

---
+++ Variables/Subtraction 3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+    printf(" enter b");
+    scanf("%d" ,&b);
+    printf("subtraction of a & b is : %d", a-b);
+    return 0;
+}
