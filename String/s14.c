f52f1ad7fa37c2734245f9211fe31dd254d02ef6
Md Kaif <mdkaif.kmclu@gmail.com>
2026-02-26 22:41:05 +0530

    check char

---
+++ String/s14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void checkChar(char str[], char ch);
+
+int main (){
+    char str[] = "kaif";
+    char ch = 'o';
+    checkChar(str,ch);
+}
+void checkChar(char str[], char ch){
+for(int i=0; str[i] !='\0'; i++){
+    if(str[i] = 'n'){
+        printf("character is present!");
+    
+
+ return ;
+    }
+            }
+printf("charcharter is NOT present :");
+}
+
