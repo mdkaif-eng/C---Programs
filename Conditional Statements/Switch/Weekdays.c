e35f0697f73d234322df83ba4367bd51d11d2467
Md Kaif <mdkaif.kmclu@gmail.com>
2026-03-03 21:30:28 +0530

    check the week days using switch and break

---
+++ Conditional Statements/Switch/Weekdays.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//Mon-1;Tues-2;Wedn-3
+int main(){
+    int day;
+    printf("enter day (1-7) : ");
+    scanf("%d", &day);
+
+    switch(day){
+        case 1 : printf("Monday\n");
+        break;
+        case 2 : printf("Tuesday\n");
+        break;
+        case 3 : printf("Wednesday\n");
+        break;
+        case 4 : printf("Thursday\n");
+        break;
+        case 5 : printf("Friday\n");
+        break;
+        case 6 : printf("Saturday\n");
+        break;
+        case 7 : printf("Sunday\n");
+        break;
+        default :printf("not valid day\n");
+    }
+    return 0;
+}
